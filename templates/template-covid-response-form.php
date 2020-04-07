8110ab99cc6a7e216390b4398f650fd4765bc2a5
Marco Chiesi <marco@blackstudio.it>
2020-04-07 20:05:22 +0200

    Added template for Covid Response submission form

---
+++ templates/template-covid-response-form.php
@@ -0,0 +1,187 @@
+<?php
+/*
+Template Name: Covid response form
+Template Post Type: page
+*/
+
+/*
+** ACF filter to customize Open Government form
+*/
+// give value to case type taxonomy field
+function bs_case_study_form_taxonomy_field($field) {
+	// get Open Gov case type term ID
+	$opsi_term = get_term_by('slug', 'opsi', 'case_type');
+	$opsi_term_id = $opsi_term->term_id;
+	//  set value
+	$field['value'] = $opsi_term_id;
+  return $field;
+}
+add_filter('acf/prepare_field/key=field_5bc60b5d239b4', 'bs_case_study_form_taxonomy_field', 20);
+// give value to Primary case type taxonomy field
+function bs_case_study_form_primary_case_field($field) {
+	$field['value'] = 'opsi';
+  return $field;
+}
+add_filter('acf/prepare_field/key=field_5be00a685e5a8', 'bs_case_study_form_primary_case_field', 20);
+
+// Hide Organisation Type field
+function bs_case_study_form_organisation_type($field) {
+  return;
+}
+add_filter('acf/prepare_field/key=field_5bc9fb9ae7390', 'bs_case_study_form_organisation_type', 20);
+
+// Hide Part of Open Gov partnership field
+function bs_case_study_form_open_gov_partnership($field) {
+  return;
+}
+add_filter('acf/prepare_field/key=field_5bca000811cdb', 'bs_case_study_form_open_gov_partnership', 20);
+
+// Innovation tags OpenGov
+function bs_case_study_form_innovation_tags_opengov_fields($field) {
+	return;
+}
+add_filter( 'acf/prepare_field/key=field_5bd734194e45a', 'bs_case_study_form_innovation_tags_opengov_fields', 20 );
+
+// Submission date
+function bs_case_study_form_submission_date_field($field) {
+	$field['wrapper']['class'] .= ' hidden';
+	return $field;
+}
+add_filter( 'acf/load_field/key=field_5cff4295a4f99', 'bs_case_study_form_submission_date_field', 20 );
+
+
+	acf_form_head();
+	get_header();
+
+	?> <style>#acf-field_5ae7ab3b5dd80-, .acf-field-5bc60b5d239b4 {display: none;}</style> <?php
+
+    global $post, $bp;
+
+	if ( isset( $_GET['edit'] ) && intval( $_GET['edit'] ) > 0 && !can_edit_acf_form( intval( $_GET['edit'] ) ) ) {
+		?>
+		<div class="col-sm-12">
+			<div class="alert alert-warning text-center">
+				<h3><?php echo __( 'Sorry, you cannot edit a case study that was submitted by someone else or a case study that has already been published. If you need to make changes to a published case study, please contact the OPSI team at', 'opsi' ); ?> <a href="mailto:opsi@oecd.org">opsi@oecd.org</a></h3>
+			</div>
+
+			<br />
+			<a href="<?php echo $bp->loggedin_user->domain . 'innovations/'; ?>" title="<?php echo __( 'Back', 'opsi' ); ?>" class="button btn btn-default flipicon">
+            <i class="fa fa-chevron-left" aria-hidden="true"></i>  <?php echo __( 'Back', 'opsi' ); ?>
+			</a>
+
+		</div>
+		<?php
+		get_footer();
+		return;
+	}
+
+
+	if ( isset( $_GET['delete'] ) && intval( $_GET['delete'] ) > 0 ) {
+
+		$can_delete_cs = can_delete_cs( intval( $_GET['delete'] ) );
+
+		if ( !can_delete_cs( intval( $_GET['delete'] ) ) ) {
+			?>
+			<div class="col-sm-12">
+				<div class="alert alert-warning text-center">
+					<h3><strong>Error!</strong> <?php echo __( 'You can not delete this case study.', 'opsi' ); ?></h3>
+				</div>
+			</div>
+			<?php
+			get_footer();
+			return;
+		} else {
+			if ( isset( $_GET['confirm'] ) && intval( $_GET['confirm'] ) == 1 ) {
+
+				if ( $can_delete_cs == 'delete' ) {
+					wp_delete_post( intval( $_GET['delete'] ) );
+				}
+
+				if ( $can_delete_cs == 'request' ) {
+					 wp_update_post( array( 'ID' => intval( $_GET['delete'] ), 'post_status' => 'pending_deletion' ) );
+				}
+				wp_safe_redirect( get_bloginfo('url') . '/members/'. $current_user->user_login . '/profile/');
+				exit();
+
+			} else {
+			?>
+				<div class="col-sm-12">
+					<div class="alert alert-warning text-center">
+						<h3><a href="<?php echo get_permalink( $post->ID ); ?>?delete=<?php echo intval( $_GET['delete'] ); ?>&confirm=1"><?php echo __( 'Please confirm deleting this case study by clicking here.', 'opsi' ); ?></a></h3>
+					</div>
+				</div>
+			<?php
+			}
+		}
+		get_footer();
+		return;
+	}
+
+    $has_sidebar = 0;
+    $layout = get_post_meta($post->ID, 'layout', true);
+    if($layout != 'fullpage' && is_active_sidebar( 'sidebar' )) {
+      $has_sidebar = 3;
+    }
+
+  ?>
+
+
+	<div class="col-sm-3 dont-col-sm-push--9">
+		<ul id="acf_steps">
+		</ul>
+
+		<?php if ( is_active_sidebar( 'sidebar_covid_response_form' )) { dynamic_sidebar( 'sidebar_covid_response_form' ); } ?>
+	</div>
+    <div class="col-sm-9 dont-col-sm-pull--3">
+
+  <?php while ( have_posts() ) : the_post(); $postid = get_the_ID(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div id="formtop"></div>
+			<div id="case_form" class="stepform">
+            <?php
+
+				$group = get_page_by_title('Covid Responses', OBJECT, 'acf-field-group');
+
+				$form_params = array(
+					'field_groups' 	=> array( $group->ID ),
+					'new_post'		=> array(
+						'post_type'		=> 'covid_response',
+						'post_status'	=> 'publish',
+						'post_author'	=> get_current_user_id(),
+						'post_content' => true,
+						'post_title' => true,
+					),
+					'submit_value'		=> __( 'Create a new Covid response', 'opsi' ),
+					'post_id'			=> 'new_post',
+					'form' 				=> true,
+					'updated_message' 	=> '<span class="alert alert-success updatedalert" role="alert">'. __("Innovation submission saved on", 'acf') .' '. date_i18n( get_option('date_format') .' '.get_option('time_format') ) .'</span>',
+					//'return' => '',
+					'html_before_fields' => '<input type="hidden" id="csf_action" name="csf_action" value="" style="display:none;"><input type="hidden" id="form_step_field" name="form_step" value="0" style="display:none;">',
+				);
+
+				if ( isset( $_GET['edit'] ) && intval( $_GET['edit'] ) > 0 ) {
+
+					$form_params['post_id'] 		= $_GET['edit'];
+					$form_params['new_post'] 		= false;
+					$form_params['submit_value'] 	= __( 'Save your Covid response', 'opsi' );
+
+
+				}
+
+				acf_form( $form_params );
+
+
+			?>
+			</div>
+          </article>
+      <?php // comments_template(); ?>
+      <?php endwhile; ?>
+
+	  <?php echo get_options_acf_fields_by_group_key(); ?>
+
+    </div>
+
+  <?php wp_reset_query(); ?>
+
+
+<?php get_footer(); ?>
