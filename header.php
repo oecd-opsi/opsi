d4d922a2d8297e4b28ed3cf89ba31910a6dd8916
Marco Chiesi <marco@blackstudio.it>
2020-08-02 13:51:02 +0200

    Added Mailchimp popup

--- header.php
+++ header.php
@@ -28,6 +28,7 @@
 
     <?php wp_head(); ?>
 
+	  <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/57778163aacf3b56841696fe1/decf6415f0daae83b4cec44d9.js");</script>
   </head>
   <body itemscope itemtype="http://schema.org/WebPage" id="opsibody" <?php body_class(); ?>>
 
