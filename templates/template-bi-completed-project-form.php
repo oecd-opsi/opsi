02efe6a2907ac11b7f07a7b09bbda3b4a0c0192e
conlaccento <antonio@blackstudio.it>
2021-05-18 23:35:43 +0200

    edit BI form based on client feedback

--- templates/template-bi-completed-project-form.php
+++ templates/template-bi-completed-project-form.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: BI pre-registration form
+Template Name: BI completed project form
 Template Post Type: page
 */
 
@@ -10,7 +10,7 @@ Template Post Type: page
 // give value to status taxonomy field
 function bs_bi_project_form_status_field($field) {
 	// get Pre-registration term ID
-	$status_term = get_term_by( 'slug', 'pre-registration', 'bi-project-status' );
+	$status_term = get_term_by( 'slug', 'completed', 'bi-project-status' );
 	$status_term_id = $status_term->term_id;
 	// set value
 	$field['value'] = $status_term_id;
@@ -21,16 +21,22 @@ add_filter('acf/prepare_field/key=field_608047a8deef6', 'bs_bi_project_form_stat
 // Retrieve Intro page content
 function bs_bi_form_first_page( $field ) {
 
-	$bi_first_page = get_field( 'bi_pre-registration_first_page', 'option' );
+	$bi_first_page = get_field( 'bi_completed_project_first_page', 'option' );
 
 	if ( !is_admin() && !empty( $bi_first_page ) && is_page( $bi_first_page ) ) {
 		$field['label'] 	= '';
-		$field['message'] 	= apply_filters( 'the_content', get_post_field( 'post_content', get_field( 'bi_pre-registration_first_page', 'option' ) ) );
+		$field['message'] 	= apply_filters( 'the_content', get_post_field( 'post_content', get_field( 'bi_completed_project_first_page', 'option' ) ) );
 	}
 
 	return $field;
 }
 add_filter( "acf/load_field/key=field_60a38051279c8", 'bs_bi_form_first_page' );
+
+// Hide Data Collection field
+function bs_project_form_data_collection($field) {
+  return;
+}
+add_filter('acf/prepare_field/key=field_60a41f69545e1', 'bs_project_form_data_collection', 20);
 
 /*
 ** Start the template
